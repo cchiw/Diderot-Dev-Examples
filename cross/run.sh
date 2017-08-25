01da7c3164fecc750023eae2620ca47445baf9fa
cchiw <charisee.chiw@gmail.com>
2017-08-25 18:34:19 -0500

    csg exs

--- cross/run.sh
+++ cross/run.sh
@@ -1,4 +1,4 @@
 /Users/chariseechiw/diderot/chiw17/bin/diderotc --exec mip_max.diderot
 ./mip_max -imgResU 200 -imgResV 200  -rayStep 0.01
-unu quantize -i out.nrrd -b 8 -o k_18.png
-open k_*.png
+unu quantize -i out.nrrd -b 8 -o nolimits/s_13_5_max.png
+#open nolimits/s_9_5_min.png
