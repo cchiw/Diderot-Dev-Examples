b63ecd3bc438fd29b9a405bde36f68cdf373186e
cchiw <charisee.chiw@gmail.com>
2017-09-06 23:13:06 -0500

    add selection

--- selection/run.sh
+++ selection/run.sh
@@ -1,6 +1,6 @@
 #/Users/chariseechiw/diderot/chiw17/bin/diderotc --exec dvr.diderot
 rm tmp*.png
-PARAMS="-camEye 10 20 5 -camAt 0 0 0 -camUp 0 0 1 -camNear -2 -camFar 2 -camFOV 8 -iresU 400 -iresV 400 -thick 0.1 -refStep 0.1 -rayStep 0.01 -isoval 0.0 -cmin -1 -cmax 1 -vol1 "../../data/parab3s.nrrd" -vol2 "../../data/vol_gquartic.nrrd""
+PARAMS="-camEye 10 20 5 -camAt 0 0 0 -camUp 0 0 1 -camNear -2 -camFar 2 -camFOV 8 -iresU 400 -iresV 400 -thick 0.1 -refStep 0.1 -rayStep 0.01 -isoval 0.0 -cmin -1 -cmax 1 -vol1 "../data/parab3s.nrrd" -vol2 "../data/vol_gquartic.nrrd""
 
 ./dvr $PARAMS -selection 1
 overrgb -i rgba.nrrd -b 1 1 1 -g 1.2 -o tmp_shape_1.png
