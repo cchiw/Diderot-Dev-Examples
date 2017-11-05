c0a11029c850c4b5f49a82b9e603370f3d20c153
cchiw <charisee.chiw@gmail.com>
2017-11-04 20:49:07 -0500

    quick sh

---
+++ readOut/PVO.sh
@@ -0,0 +1,9 @@
+/Users/chariseechiw/diderot/chiw17/bin/diderotc --exec --log  observ.diderot
+grep compile observ.log
+./observ --timing
+/Users/chariseechiw/diderot/chiw17/bin/diderotc --exec  --log --readEinDebug observ.diderot
+grep compile observ.log
+./observ --timing
+/Users/chariseechiw/diderot/chiw17/bin/diderotc --exec  --log --readEin observ.diderot
+grep compile observ.log
+./observ --timing
