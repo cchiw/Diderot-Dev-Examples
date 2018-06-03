b32c4e58fb6050462e408ed2808e84441cd5c357
cchiw <charisee.chiw@gmail.com>
2018-06-03 13:49:31 -0500

    move if else examples

--- dev/IfElse/X2/run.sh
+++
@@ -1,4 +0,0 @@
-/Users/chariseechiw/diderot/Diderot-Dev/bin/diderotc --exec --dev tmp.diderot
-./tmp
-unu save -f text -i out.nrrd -o tmp.txt
-open tmp.txt
---
+++ fn_composition/X5/assess.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -r out.nrrd ] ; then
+  echo "fail"
+  exit
+fi
+
+
+unu 2op - out.nrrd correct.nrrd \
+| unu reshape -s 1 16 \
+| unu project -a 1 -m l1 \
+| unu save -f text
+
